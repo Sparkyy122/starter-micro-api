80ca60da8f74d4951ee8e57a4c931bca9c3293f0
Sparkyy122 <113429069+Sparkyy122@users.noreply.github.com>
2023-12-27 09:42:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -4,40 +4,4 @@ http.createServer(function (req, res) {
     res.write('Yo!');
     res.end();
 }).listen(process.env.PORT || 3000);
-const mineflayer = require('mineflayer');
 
-const botArgs = {
-    host: 'Iampro13412.aternos.me',
-    port: '19077',
-    username: "Hello_world",
-    version: '1.20.1'
-};
-
-const initBot = () => {
-
-    // Setup bot connection
-    let bot = mineflayer.createBot(botArgs);
-
-    bot.on('login', () => {
-        let botSocket = bot._client.socket;
-        console.log(`Logged in to ${botSocket.server ? botSocket.server : botSocket._host}`);
-    });
-
-    bot.on('end', () => {
-        console.log(`Disconnected`);
-
-        // Attempt to reconnect
-        setTimeout(initBot, 5000);
-    });
-
-    bot.on('error', (err) => {
-        if (err.code === 'ECONNREFUSED') {
-            console.log(`Failed to connect to ${err.address}:${err.port}`)
-        }
-        else {
-            console.log(`Unhandled error: ${err}`);
-        }
-    });
-};
-
-initBot();
